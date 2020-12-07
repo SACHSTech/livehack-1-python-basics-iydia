2431d234ef1c5f21f2ba71eb010a2a86845ac4ec
iydia <lydia.he23@ycdsbk12.ca>
2020-12-07 18:21:48 +0000

    livehack 1 python basics

--- problem1.py
+++ problem1.py
@@ -1,1 +1,4 @@
-
+print("\nThis program allows you to enter a temperature in Celsius and converts it to Fahrenheit.")
+celsius = float(input("\nEnter the temperature in Celsius: "))
+fahrenheit = ((celsius*1.8) + 32)
+print("\nThat would be " + str(fahrenheit) + "° Fahrenheit.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,6 @@
-
+print("\nWelcome to the popeyes chicken distributor.")
+total_chicken = int(input("\nHow much chicken do you need to distribute?: "))
+total_students = int(input("\nHow many students do you need to distribute it amongst?: "))
+chicken_per_student = (total_chicken // total_students)
+remainder = total_chicken - (chicken_per_student*total_students)
+print("\nEach student receives " + str(chicken_per_student) + " chicken(s) and Mr. Fabroa will receive " + str(remainder) + " chicken(s).")
